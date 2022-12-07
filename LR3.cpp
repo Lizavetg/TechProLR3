b1681547650260046c753a138fcdda6ef1a45592
doradorrraaa <prokopenko.daria.2001@gmail.com>
2022-12-07 17:49:53 +0300

    3 commit Dora

--- LR3.cpp
+++ LR3.cpp
@@ -18,15 +18,15 @@ public:
 
     ~profiler() {
         steady_clock::time_point end_time_ = steady_clock::now();
-        cout << "Время выполнения блока" << endl;
+        cout << "Duration of block" << endl;
         cout << duration_cast<milliseconds>(end_time_ - start_t_).count() << "ms\n";
     }
 };
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    cout << "Ожидание 5 сек..."s << endl;
+    cout << "Waiting for 5 seconds..."s << endl;
     profiler sleep_guard;
     this_thread::sleep_for(5s);
-    cout << "Ожидание завершено"s << endl;
+    cout << "Wait is over"s << endl;
 }
